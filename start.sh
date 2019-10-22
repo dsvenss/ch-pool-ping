64fbc5230e42884c4a35c23a0ce2b404759f533c
pong-ping <pong-ping@comhem.com>
2019-10-22 19:48:00 +0200

    Fix startscript

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Starting pool-ping"
-cd /home/pi/projects/ch-pool-ping && git pull && cd src && python3 main.py > /home/pi/pool-ping.log 2>&1 &
+cd /home/pi/projects/ch-pool-ping
+git pull
+cd src
+python3 main.py > /home/pi/pool-ping.log 2>&1 &
 echo "Started pool-ping"
 exit 0
